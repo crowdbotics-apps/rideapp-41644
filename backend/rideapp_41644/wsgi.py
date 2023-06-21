46e141a4795e4d7c4ac39a0472b919a19ae3985d
Crowdbotics <team@crowdbotics.com>
2023-06-21 02:37:03 +0000

    Initial commit.

---
+++ backend/rideapp_41644/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rideapp_41644 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rideapp_41644.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rideapp-41644",
+  url: "https://rideapp-41644.botics.co",
+  api: "https://rideapp-41644.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
